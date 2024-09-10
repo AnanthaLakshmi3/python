0120682c83885c6d1582501e36afe28d7cf0b916
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-09-10 20:08:18 +0530

    Add files via upload

---
+++ calci.py
@@ -0,0 +1,16 @@
+a=10
+b=3
+c=a+b
+print('sum of {} and {} is:{}'.format(a,b,c))
+d=a-b
+print('Difference of {} and {} is:{}'.format(a,b,d))
+e=a*b
+print('Product of {} and {} is:{}'.format(a,b,e))
+f=a/b
+print('Actual Quotient when {} is divided b {} is:{}'.format(a,b,f))
+g=a%b
+print('Remainder when {} and {} is:{}'.format(a,b,g))
+h=a//b
+print('Floored Quotient when {} is divided by {} is:{}'.format(a,b,h))
+i=a**b
+print('power of {} and {} is:{}'.format(a,b,i))
