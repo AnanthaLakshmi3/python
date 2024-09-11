fd909d0ec5743334264513f704b900b22b2f17a2
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-09-11 22:04:30 +0530

    Add files via upload

---
+++ list5.py
@@ -0,0 +1,17 @@
+a=[9,0,0,0,0,4,6,6,9]
+a.sort()
+print(a)
+x=min(a)
+print(x)
+# y=max(a)
+# m=a.count(x)
+# n=a.count(y)
+# print(m,n)
+
+# for i in range(m):
+#     a.remove(x)
+# for i in range(n):
+#     a.remove(y)
+
+# print(a)
+# print(len(a))
