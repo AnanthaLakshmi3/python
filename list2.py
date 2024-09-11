f287fb752811fdcf1fd4eff876c663197e4eab66
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-09-11 22:03:50 +0530

    Add files via upload

---
+++ list2.py
@@ -0,0 +1,20 @@
+# lst=[1,2,3,4,5]
+# print(lst[-1])
+
+# list=[1,2,3,4,5]
+# print(list[1:5])
+
+# a="Hello World!"
+# print(a[-1])
+
+# a="Hello, World!"
+# print(a[1:5])
+
+# a="Hello, World!"
+# print(a[:5])
+
+# a="Hello, World!"
+# print(a[2:])
+
+a="Hello, World!"
+print(a[-5:-2])
