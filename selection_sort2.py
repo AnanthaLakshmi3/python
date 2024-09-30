dd15ccb2205424ecefe3be0507878b96ec01f5a5
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-09-30 19:55:27 +0530

    Add files via upload

---
+++ selection_sort2.py
@@ -0,0 +1,10 @@
+l=list( map(int,input().split()))
+for i in range(len(l)):
+  minimum=l[i]
+  index=i
+  for j in range(i,len(l)):
+    if minimum>l[j]:
+      minimum=l[j]
+      index=j
+      l[i],l[index]=l[index],l[i]
+print(l)
