1732e99c42b03d6118368388f3642d22f7e22d09
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-10-03 19:45:47 +0530

    Add files via upload

---
+++ DOLL.py
@@ -0,0 +1,10 @@
+# cook your dish here
+t=int(input())
+for i in range (t):
+    x,y=map(int,input().split())
+    li=list(map(int,input().split()))
+    c=0
+    for j in range(len(li)):
+        if y < li[j]:
+            c=c+1
+    print(c)
