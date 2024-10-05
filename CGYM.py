a291e2ee55c20050c38d0c17cceede3b68fe0d5c
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-10-05 18:41:50 +0530

    Add files via upload

---
+++ CGYM.py
@@ -0,0 +1,10 @@
+# cook your dish here
+t=int(input())
+for i in range(t):
+    x,y,z=map(int,input().split())
+    if x+y<=z:
+        print(2)
+    elif x<=z:
+        print(1)
+    else:
+        print(0)
