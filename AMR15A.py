7d8c88a596674b18f8fc078339dfe2db984fb943
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-10-06 19:28:25 +0530

    Add files via upload

---
+++ AMR15A.py
@@ -0,0 +1,14 @@
+# cook your dish here
+t=int(input())
+li=list(map(int,input().split()))
+even=0
+odd=0
+for i in range(len(li)):
+    if li[i]%2==0:
+        even=even+1  
+    else:
+        odd=odd+1  
+if even>odd:
+    print("READY FOR BATTLE")
+else:
+    print("NOT READY")
