eb93ce9c070413e43c43fd4721a0d280ac192c86
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-10-09 20:44:30 +0530

    Add files via upload

---
+++ SEATNUMBER.py
@@ -0,0 +1,12 @@
+# cook your dish here
+t=int(input())
+for i in range(t):
+    x=int(input())
+    if x>=1 and x<=10:
+        print("Lower Double")
+    elif x>=11 and x<=15:
+        print("Lower Single")
+    elif x>=16 and x<=25:
+        print("Upper Double")
+    else:
+        print("Upper single ")
