5b73fcc4bc08305d7fe17d19de3152ff4ca359ee
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-10-12 20:18:09 +0530

    Add files via upload

---
+++ CHEFPAGES.py
@@ -0,0 +1,8 @@
+# cook your dish here
+x,y=map(int,input().split())
+if x==0 :
+    print("https://www.codechef.com/practice")
+elif x==1 and y==0:
+    print("https://www.codechef.com/contests")
+elif x==1 and y==1:
+    print("https://discuss.codechef.com")
