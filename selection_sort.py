89e4875f0544a64a1c9aff1fca70737b1fc513ee
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-10-22 21:01:37 +0530

    Add files via upload

--- selection_sort.py
+++ selection_sort.py
@@ -1,6 +1,11 @@
-l=list(map(int,input().split()))
+#selection sort
+l=list( map(int,input().split()))
 for i in range(len(l)):
+  minimum=l[i]
+  index=i
   for j in range(i,len(l)):
-    if l[i]>l[j]:
-      l[i],l[j]=l[j],l[i]
+    if minimum>l[j]:
+      minimum=l[j]
+      index=j
+      l[i],l[index]=l[index],l[i]
 print(l)
