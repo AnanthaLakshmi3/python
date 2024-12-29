3fe78debb768d085c04ffb712eda46da0e25cccb
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-12-29 19:06:47 +0530

    Add files via upload

---
+++ CHANGEPOS.py
@@ -0,0 +1,8 @@
+# cook your dish here
+T = int(input())
+for _ in range(T):
+    sx, sy, ex, ey = map(int, input().split())
+    if sx != ex and sy != ey:
+        print(1)
+    else:
+        print(2)
