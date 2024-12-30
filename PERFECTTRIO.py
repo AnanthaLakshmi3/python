62b68ec2ea768dc65e81deb22fac6ccb44e2740b
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2024-12-30 20:03:40 +0530

    Add files via upload

---
+++ PERFECTTRIO.py
@@ -0,0 +1,8 @@
+# cook your dish here
+t=int(input())
+for i in range(t):
+    x,y,z=map(int,input().split())
+    if (x+y)==z or (y+z)==x or (x+z)==y:
+        print("YES")
+    else:
+        print("NO")
