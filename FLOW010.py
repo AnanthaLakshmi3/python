2456823b522096c93d9e45b778fce9da73dda6b6
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2025-01-24 19:40:16 +0530

    Add files via upload

---
+++ FLOW010.py
@@ -0,0 +1,12 @@
+# cook your dish here
+# cook your dish here
+for t in range(int(input())):
+    a=input()
+    if a=='B' or a=='b':
+        print('BattleShip')
+    if a=='c'or a=='C':
+        print("Cruiser")
+    if a=='f'or a=='F':
+        print('Frigate')
+    if a=='d' or a=='D':
+        print('Destroyer')
