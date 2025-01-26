a4a723356a7ce444023ff4e864e966aaa62cde96
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2025-01-26 19:37:13 +0530

    Add files via upload

---
+++ CHEFTRANS.py
@@ -0,0 +1,16 @@
+# cook your dish here
+for i in range(int(input())):
+    a,b,c=map(int,input().split())
+    if a+b>c:
+        print('train')
+    elif a+b==c:
+        print('equal')
+    else:
+        print('planebus')
+
+
+
+
+
+
+
