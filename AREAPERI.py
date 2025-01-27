0899006dd7d2fe230ede31c1e548bed4d8c359c7
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2025-01-27 20:30:35 +0530

    Add files via upload

---
+++ AREAPERI.py
@@ -0,0 +1,14 @@
+# cook your dish here
+l=int(input())
+b=int(input())
+area = l*b
+peri = 2*(l+b)
+if area<peri:
+    print("Peri")
+    print(peri)
+elif area>peri:
+    print("Area")
+    print(area)
+else:
+    print("Eq")
+    print(area)
