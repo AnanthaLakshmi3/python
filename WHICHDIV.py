455d2ca4459966c89392c17969cd81d2c828f0d2
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2025-01-29 20:32:16 +0530

    Add files via upload

---
+++ WHICHDIV.py
@@ -0,0 +1,9 @@
+# cook your dish here
+for i in range(int(input())):
+    a=int(input())
+    if a>=2000:
+        print(1)
+    elif a>=1600 and a<2000:
+        print(2)
+    elif a<1600:
+        print(3)
