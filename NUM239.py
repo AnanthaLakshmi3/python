74e1ec4af2de3a44165ea13b4b39ad2c5a808a41
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2025-01-31 20:53:54 +0530

    Add files via upload

--- NUM239.py
+++ NUM239.py
@@ -1,13 +1,11 @@
 # cook your dish here
-t=int(input())
-for i in range(t):
-    l,r=map(int,input().split())
-    c=0
-    for j in range(l,r+1):
-        rem=j%10
-        if rem in (2,3,9):
-            c=c+1
-    print(c)
-        
-        
-    
+t = int(input())
+if(t >= 1 and t <= 100):
+    for _ in range(t):
+        l, r = map(int, input().split())
+        c = 0
+        if l >= 1 and l <= r and r <= (pow(10, 5)):
+            for i in range(l, r+1, 1):
+                if i%10==2 or i%10==3 or i%10==9:
+                    c+=1
+            print(c)
