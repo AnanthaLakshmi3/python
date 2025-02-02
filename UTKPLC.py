d9ab16442a734ae0a92870440271fa49c325ce90
G.Durga Anantha Lakshmi <durgaananthalakshmigollavilli@gmail.com>
2025-02-02 19:04:52 +0530

    Add files via upload

---
+++ UTKPLC.py
@@ -0,0 +1,10 @@
+# cook your dish here
+n=int(input())
+for i in range(n):
+    a=list(input().split())
+    x=list(input().split())
+    for i in (a):
+        if i in x:
+            print(i)
+            break
+        
